2f3e773ba57249c1eccc5a50830b0437b4682168
Jack <576567826@qq.com>
2020-09-21 00:10:50 +0800

    lbp_feature modify

--- src/lbp_feature.py
+++ src/lbp_feature.py
@@ -10,23 +10,37 @@ import cv2
 
 radius = 1
 n_point = radius * 8
+
 image = cv2.imread('pic/rm.jpg')
 img_del = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 plt.subplot(111)
 plt.imshow(img_del)
 # plt.show()
 
+# 灰度图转换
 img_del = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 plt.subplot(111)
 plt.imshow(img_del, plt.cm.gray)
 plt.show()
 
+# LBP处理
 lbp = local_binary_pattern(img_del, n_point, radius)
 plt.subplot(111)
 plt.imshow(lbp, plt.cm.gray)
 plt.show()
+print(lbp)
+# max_bins = int(lbp.max() + 1)
+# filename = 'lbp.txt'
+# np.savetxt(filename, lbp, fmt='%.2f')
+
 
 '''
+def load_image(img_path):
+    image = Image.open(img_path)
+    img = np.array(image)
+    
+
+
 def lbp_texture_detect():
     im_hist = np.zeros((200, 256))
 
