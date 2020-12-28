7e941405673d2e5e95cf11f2bcffc0f98f768de4
Jack <576567826@qq.com>
2020-12-29 00:39:44 +0800

    直方图均衡化代码更新

--- src/histogram_Equalization.py
+++ src/histogram_Equalization.py
@@ -14,12 +14,12 @@ img = cv.imread(img_file)
 im_gray = cv.cvtColor(img,cv.COLOR_RGB2GRAY)    #将图像转为灰度，减少计算的维度
 cv.imshow('im_gray', im_gray)
 
-#w = im_gray.shape[0]
-#h = im_gray.shape[1]
+height = im_gray.shape[0]
+width = im_gray.shape[1]
 
 print(im_gray)
 
-p1 = plt.hist(im_gray.reshape(im_gray.size,1))
+plt.hist(im_gray.reshape(im_gray.size,1))
 plt.show()
 
 # 创建直方图
@@ -33,3 +33,29 @@ for x in range(0, im_gray.shape[0]):
         n[im_gray[x][y]] += 1
 
 print(n)
+
+#归一化
+for i in range(0, 256):
+    p[i] = n[i] / float(im_gray.size)
+
+#计算累积直方图
+c[0] = p[0]
+for i in range(1, 256):
+    c[i] = c[i - 1] + p[i]
+
+print(c)
+
+des = np.zeros((height, width), dtype=np.uint8)
+
+for x in range(0, height):
+    for y in range(0, width):
+        des[x][y] = 255 * c[im_gray[x][y]]
+
+print(des)
+
+cv.imshow('des', des)
+
+plt.hist(des.reshape(des.size, 1))
+plt.show()
+
+
